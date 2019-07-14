94029291f32f73db19465209f4acbbc6a330e901
awkwardio <mrmindshattering@gmail.com>
2019-07-14 10:19:15 -0400

    deleted psycopg2 part of settings

--- projectlego/settings.py
+++ projectlego/settings.py
@@ -76,7 +76,7 @@ WSGI_APPLICATION = 'projectlego.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'dfcqd2hmlmpr9q',
         'USER': 'rcmhhtxhhaosml',
         'PASSWORD': 'adcd82fe81470c81498932cabf8bbe656dcff15d5145ebbddeb7e435e095cf43',
